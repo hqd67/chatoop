bc962b607aa01c06d17f9b87d4db256c84dd179b
артем <artemlemur704@gmail.com>
2025-11-16 07:10:13 +0300

    Добавлен класс Message для обмена сообщениями

---
+++ ChatClient/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Message
+{
+    public string Type { get; set; }
+    public string Sender { get; set; }
+    public string Recipient { get; set; }
+    public string Text { get; set; }
+    public string FileName { get; set; }
+    public long FileSize { get; set; }
+    public string FileData { get; set; }
+    public DateTime Timestamp { get; set; }
+}
